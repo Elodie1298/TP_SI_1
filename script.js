2da95da6bdde1f181c5d73282581fea1c987dbaf
Boubou <Elodie.morin1298@gmail.com>
2019-09-28 15:17:53 +0100

    Auto stash before merge of "master" and "origin/master"

--- script.js
+++ script.js
@@ -1,7 +1,5 @@
 const plainText = document.querySelector("#Plaintext");
 const steps = document.querySelector("#Steps");
-const encryptBtn = document.querySelector("#Encrypt");
-const decryptBtn = document.querySelector("#Decrypt");
 const textArea = document.querySelector("#TextArea");
 
 function genCharArray(charA, charZ) {
