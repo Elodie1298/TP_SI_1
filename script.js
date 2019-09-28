aa1d1e86312875856de8f73e5518fac316db8c14
rbramos <rbr.ramos@gmail.com>
2019-09-28 09:13:03 +0100

    variable definition

---
+++ script.js
@@ -0,0 +1,7 @@
+const plainText = document.querySelector("#Plaintext")
+const steps = document.querySelector("#Steps")
+const encryptBtn = document.querySelector("#Encrypt")
+const decryptBtn = document.querySelector("#Decrypt")
+const textArea = document.querySelector("#TextArea")
+
+
